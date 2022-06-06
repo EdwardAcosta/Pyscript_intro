bf068e2fc2942559b1ce6a6a6af9e75e969ecb50
Edward Rene Acosta <Eacosta2@satx.rr.com>
2022-06-05 19:45:58 -0500

    Initial upload

---
+++ main.py
@@ -0,0 +1,21 @@
+ 
+import numpy as np
+import random
+from utils import add_class
+
+output_el = Element('output').element
+
+arr = np.array([22,58,87,34,5])
+
+#pyscript.write('output',F"{arr}")
+output_el.innerHTML = f"{arr}"
+
+def shuffle_array(*args):
+    #Shuffle
+    shuffled = sorted(arr, key=lambda k: random.random())
+
+    #change color
+    add_class(output_el, "text-blue-500")    
+    #pyscript.write('output', shuffled)
+    
+    output_el.innerHTML = shuffled
---
+++ utils.py
@@ -0,0 +1,8 @@
+
+def add_class(element, class_name):
+    element.classList.add(class_name)
+    
+    
+
+            
+    
